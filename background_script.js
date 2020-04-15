5ed2a8c90c683f3d30a10539fbbe9621e8dd9896
John Hutchins <johnwallshutchins@gmail.com>
2020-04-15 10:48:07 -0400

    fixed and tested

---
+++ background_script.js
@@ -0,0 +1,10 @@
+
+const devSuffix = '/_ui/common/apex/debug/ApexCSIPage'
+browser.browserAction.onClicked.addListener(function () {
+    alert('did anything log? ')
+    browser.tabs.query({ currentWindow: true, active: true }, tabs => {
+        let url = tabs[0].url
+        const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i)
+        return browser.tabs.create({ 'url': matches[0] + devSuffix })
+    })
+})
