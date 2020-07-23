e7424352e435719a3264c2d0aae25d18d73437ed
John Hutchins <johnwallshutchins@gmail.com>
2020-07-23 07:58:56 -0400

    progress toward debug

--- background_script.js
+++ background_script.js
@@ -1,12 +1,14 @@
 const devSuffix = '/_ui/common/apex/debug/ApexCSIPage'
+const errPage = '/home.html'
 browser.browserAction.onClicked.addListener(function () {
     browser.tabs.query({ currentWindow: true, active: true }, tabs => {
         let url = tabs[0].url
         const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i)
-        console.log(typeof matches[0])
         if (!matches[0].includes('salesforce')) {
             console.error("THIS IS AN ERROR. NOT SALESFORCE")
+            browser.tabs.create({ 'url': errPage })
+        } else {
+            return browser.tabs.create({ 'url': matches[0] + devSuffix })
         }
-        return browser.tabs.create({ 'url': matches[0] + devSuffix })
     })
 })
