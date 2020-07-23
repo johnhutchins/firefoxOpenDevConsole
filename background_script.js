f6780aa45e5a6fbdb5cfce5e08fbb1d8149dec65
John Hutchins <johnwallshutchins@gmail.com>
2020-07-23 11:13:05 -0400

    fix

--- background_script.js
+++ background_script.js
@@ -1,13 +1,7 @@
 const devSuffix = '/_ui/common/apex/debug/ApexCSIPage'
 browser.browserAction.onClicked.addListener(function () {
     browser.tabs.query({ currentWindow: true, active: false }, tabs => {
-        tabs.forEach((tab) => {
-            if (tab.url.includes('salesforce') || tab.url.includes('lightning.force')) {
-                url = tab.url
-            } else {
-                url = tabs[0].url
-            }
-        })
+        let url = tabs[0].url
         const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i)
         return browser.tabs.create({ 'url': matches[0] + devSuffix })
     })
