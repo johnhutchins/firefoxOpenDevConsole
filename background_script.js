df2d829c7f37d5b5e259ef26df92830de089cfab
John Hutchins <johnwallshutchins@gmail.com>
2020-07-23 10:48:38 -0400

    any open salesforce tab

--- background_script.js
+++ background_script.js
@@ -1,20 +1,14 @@
 const devSuffix = '/_ui/common/apex/debug/ApexCSIPage'
-const errPage = '/home.html'
 browser.browserAction.onClicked.addListener(function () {
-    browser.tabs.query({ currentWindow: true, active: true }, tabs => {
-        let url = tabs[0].url
+    browser.tabs.query({ currentWindow: true, active: false }, tabs => {
+        tabs.forEach((tab) => {
+            if (tab.url.includes('salesforce')) {
+                url = tab.url
+            } else {
+                url = tabs[0].url
+            }
+        })
         const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i)
-        if (!matches[0].includes('salesforce')) {
-            console.error("THIS IS AN ERROR. NOT SALESFORCE")
-            browser.notifications.create(cakeNotification, {
-                "type": "basic",
-                //"iconUrl": browser.runtime.getURL("icons/cake-96.png"),
-                "title": "Time for cake!",
-                "message": "Something something cake"
-            });
-            //browser.tabs.create({ 'url': errPage })
-        } else {
-            return browser.tabs.create({ 'url': matches[0] + devSuffix })
-        }
+        return browser.tabs.create({ 'url': matches[0] + devSuffix })
     })
 })
